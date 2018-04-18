ac2c5e857524f1f4f29307ce8ad6b734b935889d
Christian Williams <williamschristian1521@gmail.com>
2018-04-18 04:52:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,17 +20,17 @@ function divide(a, b)
 
 function inc(n)
 {
-  return n++
+  return ++n
 }
 
 function dec(n)
 {
-  return n--
+  return --n
 }
 
 function makeInt(n)
 {
-  return parseInt(n)
+  return parseInt(n, 10)
 }
 
 function preserveDecimal(n)
